da015dbade33b8ef7f97336b318108ad544d2cce
s24223 <127308001+s24223@users.noreply.github.com>
2025-08-03 19:40:14 +0200

    frontend: changed appBar's look

--- frontend/app/components/AppBar.tsx
+++ frontend/app/components/AppBar.tsx
@@ -10,34 +10,43 @@ const AppBar = () => {
   const { data: session } = useSession();
 
   return (
-    <div>
-      
-      <button onClick={() => { window.location.href = '/' }}>MainPage</button>
-      <ReturnButton/>
-      <ThemeToggle/>
-      {session?.user && ( // Pokaż przycisk tylko, jeśli użytkownik jest zalogowany, trzeba bedzie sprawdzac jakoś potem czy ma company czy nie dla dalszych czynności
-        <Link href="/recruitments" passHref>
-          <button className="px-4 py-2 rounded-md bg-purple-500 text-white hover:bg-purple-600 transition duration-200">
-            Recruitments
+    <div className="flex justify-between items-center px-4 py-3 bg-white dark:bg-gray-800 shadow-md">
+      <div className="flex items-center space-x-4">
+        <button onClick={() => { window.location.href = '/' }}>MainPage</button>
+        <ReturnButton/>
+        <ThemeToggle/>
+      </div>
+      <div className="flex items-center space-x-4">
+        {session?.user && ( // Pokaż przycisk tylko, jeśli użytkownik jest zalogowany, trzeba bedzie sprawdzac jakoś potem czy ma company czy nie dla dalszych czynności
+          <Link href="/recruitments" passHref>
+            <button className="px-4 py-2 rounded-md bg-purple-500 text-white hover:bg-purple-600 transition duration-200">
+              Recruitments
+            </button>
+          </Link>
+        )}
+        {session?.user? (
+          <>
+          <button onClick={() => { window.location.href = '/profile' }} className="px-4 py-2 rounded-md text-gray-800 dark:text-white bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 transition duration-200">
+            Profile
           </button>
-        </Link>
-      )}
-      {session?.user? (
-        <>
-        <button onClick={() => { window.location.href = '/profile' }}>Profile</button>
-        <button onClick={() => {
-          signOut()
-          let router = useRouter()
-          router.push("/")
-          }}>Sign Out</button>
-        </>
-      ) : (
-        <>
-          <button onClick={() => signIn()}>Sign In</button>
-          <button onClick={() => { window.location.href = '/register' }}>Sign Up</button>
-        </>
-      )}
-      
+          <button onClick={() => { signOut()
+            let router = useRouter()
+            router.push("/") }} 
+            className="px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600 transition duration-200">
+              Sign Out
+            </button>
+          </>
+        ) : (
+          <>
+            <button onClick={() => signIn()} className="px-4 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition duration-200">
+              Sign In
+            </button>
+            <button onClick={() => { window.location.href = '/register' }}  className="px-4 py-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-600 transition duration-200">
+              Sign Up
+            </button>
+          </>
+        )}
+      </div>
     </div>
   )
 }
